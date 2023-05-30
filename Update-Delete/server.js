b3e2d8bab16f4f949337d8c3ff548b452c54d9f5
Shanzzz <shahnazafreen9849@gmail.com>
2023-05-30 10:40:32 -0400

    completed activity

--- Update-Delete/server.js
+++ Update-Delete/server.js
@@ -2,15 +2,14 @@ const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
-const app= expres();
+const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-sequelize.sync({force:false}).then(() => {
-    app.listen(PORT, () => console.log('Now Listening'));
+sequelize.sync({ force: flase }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
 });
-
