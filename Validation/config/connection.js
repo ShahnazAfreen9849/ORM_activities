f42c7ba58a7298de806afdb6b48a3b790d632424
Shanzzz <shahnazafreen9849@gmail.com>
2023-05-31 13:46:25 -0400

    completed activity

--- Validation/config/connection.js
+++ Validation/config/connection.js
@@ -1,64 +1,13 @@
-const sequelize = require('../config/connection');
-const User = require('../models/User.js');
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-test('Checks for null values',async () => {
-    const user1 = {};
-    const user2 = {
-        username: 'test',
-        email: 'test@test.com',
-        password: '11111111111',
-
-    };
-
-    const newUser1 = User.build(user1);
-    const newUser2 = User.build(user2);
-
-    await expect(newUser1.validate()).rejects.toThrow('notNull');
-    await expect(newUser2.validate()).resolves.not.toThrow();
-
-
-});
-
-test('Checks for short passwords',async () =>{
-    const user1= {
-        username: 'test',
-        email: 'test@test.com',
-        password: '123',
-
-    };
-
-    const user2= {
-        username: 'test',
-        email: 'test@test.com',
-        password: 'password123',
-    };
-    const newUser1 = User.build(user1);
-    const newUser2 = User.build(user2);
-
-    await expect(newUser1.validate()).rejects.toThrow(
-        'Validation len on password failed'
-    );
-    await expect(newUser2.validate().resolves.not.toThrow());
-});
-
-test('Checks for alphanumeric username', async () => {
-    const user1 = {
-      username: 'test_123',
-      email: 'test@test.com',
-      password: '123',
-    };
-  
-    const user2 = {
-      username: 'test',
-      email: 'test@test.com',
-      password: 'password123',
-    };
-  
-    const newUser1 = User.build(user1);
-    const newUser2 = User.build(user2);
-  
-    await expect(newUser1.validate()).rejects.toThrow(
-      'Validation isAlphanumeric on username failed'
-    );
-    await expect(newUser2.validate()).resolves.not.toThrow();
-  });
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dailect: 'mysql',
+    port: 3306,
+  }
+);
